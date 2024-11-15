22eab7190f20dcf6da1fbbd611342d150d0fd3cd
劉人豪🐮和李美霖☁️的無敵可愛大mac🍔 <limeilin@limeilindeMacBook-Pro.local>
2024-11-15 13:19:19 +0800

    next step: add react

--- non_code/main_app/views.py
+++ non_code/main_app/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+
 
 # Create your views here.
 #Views is a request handler
 
-def say_hello(request):
-    return HttpResponse("Hi!!!")
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        messages.success(request, f"username={username}, password={password}")
+        
+
+    # Check if credentials match
+    #if username in users and users[username] == password:
+    #    return f"Welcome, {username}!"
+    #else:
+    #    return "Invalid username or password.", 401
+    return render(request, "login.html")
+
+def home(request):
+    return HttpResponse("HI!!!")
